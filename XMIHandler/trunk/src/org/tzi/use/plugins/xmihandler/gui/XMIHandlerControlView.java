163b21d8c2008faf2138ef4841a11a6d7f3ad24c
huseynli <huseynli@e95982d2-0d12-e879-a455-8ce5a0885042>
2011-09-20 19:36:49 +0000

    test import export works
    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use_plugins@2515 e95982d2-0d12-e879-a455-8ce5a0885042

--- XMIHandler/trunk/src/org/tzi/use/plugins/xmihandler/gui/XMIHandlerControlView.java
+++ XMIHandler/trunk/src/org/tzi/use/plugins/xmihandler/gui/XMIHandlerControlView.java
@@ -1,25 +1,35 @@
 package org.tzi.use.plugins.xmihandler.gui;
 
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collections;
 
+import javax.swing.AbstractAction;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EAttribute;
-import org.eclipse.emf.ecore.EClass;
-import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EcoreFactory;
-import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.emf.ecore.xmi.XMIResource;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.uml2.uml.AggregationKind;
+import org.eclipse.uml2.uml.Association;
+import org.eclipse.uml2.uml.LiteralUnlimitedNatural;
+import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.PrimitiveType;
+import org.eclipse.uml2.uml.Property;
+import org.eclipse.uml2.uml.Type;
+import org.eclipse.uml2.uml.UMLPackage;
+import org.eclipse.uml2.uml.UMLFactory;
+import org.eclipse.uml2.uml.resource.UMLResource;
 import org.tzi.use.gui.main.MainWindow;
 import org.tzi.use.main.Session;
 import org.tzi.use.uml.mm.MAssociation;
@@ -35,88 +45,251 @@ import sun.awt.VerticalBagLayout;
 public class XMIHandlerControlView extends JDialog implements StateChangeListener, ChangeListener{
 
 	private Session session;
+	private static File fileToProcess = null;
 	
-	class CustomXMIResourceFactoryImpl extends XMIResourceFactoryImpl
-	{
-	  @Override
-	  public Resource createResource(URI uri) {
-	    XMIResourceFactoryImpl resFactory = new XMIResourceFactoryImpl();
-	    XMIResource resource = (XMIResource) resFactory.createResource(uri);
-	    resource.setXMIVersion("1.2");
-	    return resource;
-	  }
-	  
-	}
+  private JTextArea fileNameTextArea;
+  private ButtonGroup buttonGroup;
+  private JPanel selectPanel;
+  private JButton selectBtn;
+  private JPanel processPanel;
+  private JRadioButton importRadioBtn;
+  private JRadioButton exportRadioBtn;
+  private JButton processBtn;  
 	
-	public XMIHandlerControlView(MainWindow parent, Session session) {
+	public XMIHandlerControlView(MainWindow parent, Session theSession) {
 		super(parent, "XMIHandler Control");
-		this.session = session;
+		this.session = theSession;
 		session.addChangeListener(this);
 		initGUI();
-		testEMF(session.system().model());
 	}
 	
-	private void testEMF(MModel model) {
+	public static void main (String[] args) {
+	  new XMIHandlerControlView(null, null);
+//	  testXMIExport ();
+//    testXMIImport ();	  
+	}
+	
+	private static void exportXMI(File file, MModel model) {
 	// Create a resource set.
 	  ResourceSet resourceSet = new ResourceSetImpl();
-
-	  // Register the default resource factory -- only needed for stand-alone!
-	  resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
-	    Resource.Factory.Registry.DEFAULT_EXTENSION, new CustomXMIResourceFactoryImpl());
+	  
+	  UMLPackage.eINSTANCE.getName();
+	  
+	  resourceSet.getPackageRegistry().put(UMLPackage.eNS_URI, UMLPackage.eINSTANCE);
+	  
+	  resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(Resource.Factory.Registry.DEFAULT_EXTENSION, UMLResource.Factory.INSTANCE);
+ 
+	  URI uri = URI.createURI("jar:file:/home/stalker/uni/Masterarbeit/xmihandler_repo/lib/org.eclipse.uml2.uml.resources_3.0.0.v200906011111.jar!/");
+	  resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.LIBRARIES_PATHMAP), uri.appendSegment("libraries").appendSegment(""));
+	  resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.METAMODELS_PATHMAP), uri.appendSegment("metamodels").appendSegment(""));
+	  resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.PROFILES_PATHMAP), uri.appendSegment("profiles").appendSegment(""));
 	  
 	  // Get the URI of the model file.
-	  URI fileURI = URI.createFileURI(new File("mylibrary.xmi").getAbsolutePath());
+	  URI fileURI = URI.createFileURI(file.getAbsolutePath());
 	  System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>" + fileURI.path());
 
 	  // Create a resource for this file.
 	  Resource resource = resourceSet.createResource(fileURI);
 	  
-	  for (MClass mClass : model.classes())
-	  {
-      System.out.println ("Class: " + mClass.name());	    
-	    for (MAttribute mAttribute : mClass.attributes())
-	    {
-	      System.out.println (" " + mAttribute.name());	      
-	    }
-      for (MAssociation mAssociation : mClass.associations())
-      {
-        System.out.println (" " + mAssociation.name());       
-      }	    
+    final Model companyModel = UMLFactory.eINSTANCE.createModel();
+    companyModel.setName(model.name());
+    
+    final org.eclipse.uml2.uml.Class sampleClass = UMLFactory.eINSTANCE.createClass();
+    sampleClass.setName("Test");
+    
+    for (MClass mClass : model.classes())                                                                                                                                                       
+    {                                                                                                                                                                                           
+      System.out.println ("Class: " + mClass.name());                                                                                                                                           
+      for (MAttribute mAttribute : mClass.attributes())                                                                                                                                         
+      {                                                                                                                                                                                         
+        System.out.println (" " + mAttribute.name());                                                                                                                                           
+      }                                                                                                                                                                                         
+      for (MAssociation mAssociation : mClass.associations())                                                                                                                                   
+      {                                                                                                                                                                                         
+        System.out.println (" " + mAssociation.name());                                                                                                                                         
+      }                                                                                                                                                                                         
+                                                                                                                                                                                                
+    }              	  
 
-	  }
+	  org.eclipse.uml2.uml.Class employeeClass = companyModel.createOwnedClass("Employee", false);
+    org.eclipse.uml2.uml.Class departmentClass = companyModel.createOwnedClass("Department", false);	  
+    org.eclipse.uml2.uml.Class projectClass = companyModel.createOwnedClass("Project", false);
+    
+    PrimitiveType stringPrimitiveType = companyModel.createOwnedPrimitiveType("String");
+    PrimitiveType integerPrimitiveType = companyModel.createOwnedPrimitiveType("Integer");    
+    
+    employeeClass.createOwnedAttribute("name", stringPrimitiveType);
+    employeeClass.createOwnedAttribute("salary", integerPrimitiveType);
+    
+    departmentClass.createOwnedAttribute("name", stringPrimitiveType);
+    departmentClass.createOwnedAttribute("location", stringPrimitiveType);
+    departmentClass.createOwnedAttribute("budget", integerPrimitiveType);    
+    
+    projectClass.createOwnedAttribute("name", stringPrimitiveType);
+    projectClass.createOwnedAttribute("budget", integerPrimitiveType);    
 	  
-	  EcoreFactory ecoreFactory = EcoreFactory.eINSTANCE;
-	  EClass purchaseOrderClass = ecoreFactory.createEClass();
-	  purchaseOrderClass.setName("PurchaseOrder");
-	  EAttribute shipTo = ecoreFactory.createEAttribute();
-	  shipTo.setName("shipTo");
-	  shipTo.setEType(EcorePackage.Literals.ESTRING);
-	  purchaseOrderClass.getEStructuralFeatures().add(shipTo);
+    org.eclipse.uml2.uml.Association worksInAssoc = 
+      employeeClass.createAssociation(true, AggregationKind.NONE_LITERAL, "Department", 0, LiteralUnlimitedNatural.UNLIMITED, 
+        departmentClass, false, AggregationKind.NONE_LITERAL, "Employee", 1, LiteralUnlimitedNatural.UNLIMITED);
+    
+    worksInAssoc.setName("WorksIn");
+    
+    org.eclipse.uml2.uml.Association worksOnAssoc = 
+      employeeClass.createAssociation(true, AggregationKind.NONE_LITERAL, "Project", 0, LiteralUnlimitedNatural.UNLIMITED, 
+        projectClass, false, AggregationKind.NONE_LITERAL, "Employee", 0, LiteralUnlimitedNatural.UNLIMITED);
+    
+    worksOnAssoc.setName("WorksOn");
+    
+    org.eclipse.uml2.uml.Association controlsAssoc = 
+      departmentClass.createAssociation(true, AggregationKind.NONE_LITERAL, "Project", 1, 1, 
+        projectClass, false, AggregationKind.NONE_LITERAL, "Department", 0, LiteralUnlimitedNatural.UNLIMITED);
+    
+    controlsAssoc.setName("Controls");    
+    
+	  resource.getContents().add(companyModel);
 	  
-	  EPackage poPackage = ecoreFactory.createEPackage();
-	  poPackage.setName("po");
-	  poPackage.setNsPrefix("po");
-	  poPackage.setNsURI("http://www.example.com/SimplePO");
-	  poPackage.getEClassifiers().add(purchaseOrderClass);
-	  
-	  resource.getContents().add(poPackage);
-	  
-	  // Add the book and writer objects to the contents.
-	  //resource.getContents().add(book);
 	  // Save the contents of the resource to the file system.
 	  try
 	  {
 	    resource.save(Collections.EMPTY_MAP);
 	  }
-	  catch (IOException e) {}	  
+	  catch (IOException e) {} 
 	}
+	
+  private static void importXMI(File file, MModel model) {
+    // Create a resource set.
+    ResourceSet resourceSet = new ResourceSetImpl();
+    
+    UMLPackage.eINSTANCE.getName();
+    
+    resourceSet.getPackageRegistry().put(UMLPackage.eNS_URI, UMLPackage.eINSTANCE);
+    
+    resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(Resource.Factory.Registry.DEFAULT_EXTENSION, UMLResource.Factory.INSTANCE);
+ 
+    URI uri = URI.createURI("jar:file:/home/stalker/uni/Masterarbeit/xmihandler_repo/lib/org.eclipse.uml2.uml.resources_3.0.0.v200906011111.jar!/");
+    resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.LIBRARIES_PATHMAP), uri.appendSegment("libraries").appendSegment(""));
+    resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.METAMODELS_PATHMAP), uri.appendSegment("metamodels").appendSegment(""));
+    resourceSet.getURIConverter().getURIMap().put(URI.createURI(UMLResource.PROFILES_PATHMAP), uri.appendSegment("profiles").appendSegment(""));
+    
+    // Get the URI of the model file.
+    URI fileURI = URI.createFileURI(file.getAbsolutePath());
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>" + fileURI.path());
+
+    // Create a resource for this file.
+    Resource resource = resourceSet.getResource(fileURI, true);
+    try
+    {
+      resource.load(Collections.EMPTY_MAP);
+    }
+    catch (IOException e) {}    
+
+    final Model companyModel = (Model)resource.getContents().get(0);
+    System.out.println (companyModel.getName());
+        
+    for (Type type : companyModel.getOwnedTypes())
+    {
+      System.out.println (type.getQualifiedName());
+      
+      if (type instanceof org.eclipse.uml2.uml.Class)
+      {
+        for (Property prop : ((org.eclipse.uml2.uml.Class) type).getAllAttributes())
+        {
+          System.out.println (prop.getQualifiedName());
+        }
+      }
+      if (type instanceof Association)
+      {
+        for (Property t : ((Association)type).getAllAttributes())
+        {
+          System.out.println ("---------" + t.getLowerValue().stringValue());
+          System.out.println ("---------" + t.getUpperValue().stringValue());
+        }
+      }
+    }
+    
+  }
 	
 	private void initGUI() {
 		JPanel backPanel = new JPanel(new VerticalBagLayout());
 		this.getContentPane().add(backPanel);
+		
+	  fileNameTextArea = new JTextArea();
+	  buttonGroup = new ButtonGroup();
+	  selectPanel = new JPanel();
+	  selectBtn = new JButton();
+	  processPanel = new JPanel();
+	  importRadioBtn = new JRadioButton();
+	  exportRadioBtn = new JRadioButton();
+	  processBtn = new JButton();
+		
+    selectPanel.setName("selectPanel"); 
+    selectPanel.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+
+    fileNameTextArea.setText("No file selected"); 
+    fileNameTextArea.setName("fileNameTextArea");
+    selectPanel.add(fileNameTextArea);
+    fileNameTextArea.setRows(5);
+    fileNameTextArea.setColumns(25);
+    fileNameTextArea.setEditable(false);
+    fileNameTextArea.setLineWrap(true);
+    
+    selectBtn.setText("Select File"); 
+    selectBtn.setName("selectBtn"); 
+    selectPanel.add(selectBtn);
+    
+    selectBtn.addActionListener(new AbstractAction() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        JFileChooser fc = new JFileChooser();        
+        int returnVal = fc.showOpenDialog(null);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            fileToProcess = fc.getSelectedFile();
+            fileNameTextArea.setText(fileToProcess.getAbsolutePath());
+            //This is where a real application would open the file.
+        } else {
+        }
+      }
+    });    
+
+    backPanel.add(selectPanel);
+
+    processPanel.setName("processPanel"); 
+    processPanel.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.LEFT));
+
+    buttonGroup.add(importRadioBtn);
+    importRadioBtn.setText("Import"); 
+    importRadioBtn.setName("importRadioBtn"); 
+    processPanel.add(importRadioBtn);
+
+    buttonGroup.add(exportRadioBtn);
+    exportRadioBtn.setSelected(true);
+    exportRadioBtn.setText("Export"); 
+    exportRadioBtn.setName("exportRadioBtn"); 
+    processPanel.add(exportRadioBtn);
+
+    processBtn.setText("Process"); 
+    processBtn.setName("processBtn"); 
+    processPanel.add(processBtn);
+    
+    processBtn.addActionListener(new AbstractAction() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (exportRadioBtn.isSelected())
+        {
+          exportXMI(fileToProcess, session.system().model());          
+        } else if (importRadioBtn.isSelected()) {
+          importXMI(fileToProcess, session.system().model());          
+        }
+      }
+    });    
+
+    backPanel.add(processPanel);		
+
 		this.pack();
-    this.setSize(350, 250);		
-		this.setMinimumSize(this.getSize());
+	
+		this.setMinimumSize(this.getPreferredSize());
+		this.setVisible(true);
 	}
 
 	@Override
