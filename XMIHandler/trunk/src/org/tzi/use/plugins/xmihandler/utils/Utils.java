c429b0830a0a5e3287e6c93308b3e36991f2ba3c
huseynli <huseynli@e95982d2-0d12-e879-a455-8ce5a0885042>
2012-05-19 18:44:42 +0000

    added more missing jars
    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use_plugins@3352 e95982d2-0d12-e879-a455-8ce5a0885042

--- XMIHandler/trunk/src/org/tzi/use/plugins/xmihandler/utils/Utils.java
+++ XMIHandler/trunk/src/org/tzi/use/plugins/xmihandler/utils/Utils.java
@@ -21,6 +21,7 @@ import org.eclipse.uml2.uml.resource.UMLResource;
 import org.eclipse.uml2.uml.resource.XMI212UMLResource;
 import org.eclipse.uml2.uml.resource.XMI2UMLExtendedMetaData;
 import org.eclipse.uml2.uml.resource.XMI2UMLResource;
+import org.eclipse.uml2.uml.resources.util.UMLResourcesUtil;
 import org.tzi.use.uml.mm.ModelFactory;
 import org.tzi.use.util.Log;
 
@@ -127,6 +128,8 @@ public class Utils {
 
       uriMap.putAll(UML22UMLExtendedMetaData.getURIMap());
       uriMap.putAll(XMI2UMLExtendedMetaData.getURIMap());
+      
+      UMLResourcesUtil.init(resourceSet);
     }
 
     return resourceSet;
