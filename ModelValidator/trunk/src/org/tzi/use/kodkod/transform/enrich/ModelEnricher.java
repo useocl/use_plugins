4269e9632dcf9e537e291f0dd45cb84da389451c
lhamann <lhamann@e95982d2-0d12-e879-a455-8ce5a0885042>
2013-10-31 13:01:52 +0000

    - Restart of the trunk
    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use_plugins@4655 e95982d2-0d12-e879-a455-8ce5a0885042

--- ModelValidator/trunk/src/org/tzi/use/kodkod/transform/enrich/ModelEnricher.java
+++
@@ -1,9 +0,0 @@
-package org.tzi.use.kodkod.transform.enrich;
-
-import org.tzi.kodkod.model.iface.IModel;
-import org.tzi.use.uml.sys.MSystem;
-
-public interface ModelEnricher {
-
-	public void enrichModel(MSystem mSystem,IModel model);
-}
