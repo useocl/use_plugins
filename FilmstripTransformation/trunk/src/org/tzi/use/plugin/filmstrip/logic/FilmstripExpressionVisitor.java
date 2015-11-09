9daddbf98d2b4d732f2755ba78b2422787842f3b
fhilken <fhilken@e95982d2-0d12-e879-a455-8ce5a0885042>
2015-11-09 12:36:45 +0000

    - fixed transformation of allInstances
    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use_plugins@5776 e95982d2-0d12-e879-a455-8ce5a0885042

--- FilmstripTransformation/trunk/src/org/tzi/use/plugin/filmstrip/logic/FilmstripExpressionVisitor.java
+++ FilmstripTransformation/trunk/src/org/tzi/use/plugin/filmstrip/logic/FilmstripExpressionVisitor.java
@@ -227,60 +227,72 @@ public class FilmstripExpressionVisitor implements ExpressionVisitor {
 	
 	@Override
 	public void visitAllInstances(ExpAllInstances exp) {
-		/*
-		 * Transform <Class>.allInstances() into
-		 * self.snapshot.<Class.asRolename()>
-		 */
 		//TODO incompatible with anything other than Class.allInstances (e.g. Association.allInstances)
 		MClass expType = (MClass) mc.mapType(exp.getSourceType());
+		
 		if(selfVariables.size() < 1){
 			throw new TransformationException("No self variable found for ExpAllInstances");
 		}
 		VarDecl selfDecl = selfVariables.varDecl(0);
 		Expression self = new ExpVariable(selfDecl.name(), selfDecl.type());
 		
+		MClass snapshotClass = model.getClass(FilmstripModelConstants.SNAPSHOT_CLASSNAME);
+		Expression snapshot;
+		
 		switch (type) {
 		case CLASSINVARIANT:
 		case OPERATION:
 		case SOIL:
-			// no changes required
+			// navigate from self to snapshot, self = random object in correct snapshot
+			MClass snapElement = model.getClass(FilmstripModelConstants.SNAPSHOTITEM_CLASSNAME);
+			MAssociation assoc = model.getAssociation(FilmstripModelConstants.SNAPSHOTELEMENT_ASSOCNAME);
+			
+			MAssociationEnd sourceEnd = assoc.associationEndsAt(snapElement).iterator().next();
+			MAssociationEnd destEnd = assoc.associationEndsAt(snapshotClass).iterator().next();
+			
+			try {
+				snapshot = new ExpNavigation(self, sourceEnd, destEnd, Collections.<Expression>emptyList());
+			} catch (ExpInvalidException e1) {
+				throw new TransformationException("ExpAllInstances: Unable to navigate to snapshot class", e1);
+			}
 			break;
 		case PRECONDITION:
 		case POSTCONDITION:
-			self = new ExpVariable(FilmstripModelConstants.OPC_SELF_VARNAME, expType);
-			if(type == ExpressionType.POSTCONDITION && !exp.isPre()){
-				self = FilmstripUtil.handlePredSucc(self, false, knownVariables);
+			// self is the operation call object
+			String opName = (type == ExpressionType.POSTCONDITION && !exp.isPre()) ? "succ" : "pred";
+			MOperation op = ((MClass) selfDecl.type()).operation(opName, true);
+			
+			try {
+				snapshot = new ExpObjOp(op, new Expression[]{ self });
+			} catch (ExpInvalidException e) {
+				throw new TransformationException("ExpAllInstances: Unable to call " + StringUtil.inQuotes(opName)
+						+ " on variable " + StringUtil.inQuotes(selfDecl.name())
+						+ " of type " + StringUtil.inQuotes(selfDecl.type()), e);
 			}
 			break;
 		default:
-			throw new TransformationException("Unkown expression type " + StringUtil.inQuotes(type));
+			throw new TransformationException("ExpAllInstances: Unkown expression type " + StringUtil.inQuotes(type));
 		}
 		
-		// navigate to snapshot and back to the class
-		MClass snapshot = model.getClass(FilmstripModelConstants.SNAPSHOT_CLASSNAME);
-		MClass snapElement = model.getClass(FilmstripModelConstants.SNAPSHOTITEM_CLASSNAME);
-		MAssociation genAssoc = model.getAssociation(FilmstripModelConstants.SNAPSHOTELEMENT_ASSOCNAME);
-		MAssociation assoc = model.getAssociation(FilmstripModelConstants
-				.makeSnapshotClsAssocName(expType.name()));
+		// navigate from snapshot to expType
+		MAssociation assoc2 = model.getAssociation(FilmstripModelConstants.makeSnapshotClsAssocName(expType.name()));
 		
-		MAssociationEnd sourceEnd = genAssoc.associationEndsAt(snapElement).iterator().next();
-		MAssociationEnd snapshotEndTo = genAssoc.associationEndsAt(snapshot).iterator().next();
-		MAssociationEnd snapshotEndFrom = assoc.associationEndsAt(snapshot).iterator().next();
-		MAssociationEnd finalEnd = assoc.associationEndsAt(expType).iterator().next();
-		ExpNavigation toSnapshotNav;
-		ExpNavigation fromSnapshotNav;
+		MAssociationEnd sourceEnd2 = assoc2.associationEndsAt(snapshotClass).iterator().next();
+		MAssociationEnd destEnd2 = assoc2.associationEndsAt(expType).iterator().next();
+		
+		Expression nav;
 		try {
-			toSnapshotNav = new ExpNavigation(self, sourceEnd, snapshotEndTo,
-					Collections.<Expression> emptyList());
-			fromSnapshotNav = new ExpNavigation(toSnapshotNav, snapshotEndFrom,
-					finalEnd, Collections.<Expression> emptyList());
+			nav = new ExpNavigation(snapshot, sourceEnd2, destEnd2, Collections.<Expression>emptyList());
 		} catch (ExpInvalidException ex) {
-			throw new TransformationException("ExpAllInstances", ex);
+			throw new TransformationException("ExpAllInstances: Unable to navigate from snapshot to objects", ex);
 		}
 		
-		copyExpressionDetails(exp, toSnapshotNav);
-		copyExpressionDetails(exp, fromSnapshotNav);
-		elements.push(fromSnapshotNav);
+		if(type == ExpressionType.POSTCONDITION && exp.isPre()){
+			nav = FilmstripUtil.handlePredSucc(nav, false, knownVariables);
+		}
+		
+		copyExpressionDetails(exp, nav);
+		elements.push(nav);
 	}
 
 	@Override
--- FilmstripTransformation/trunk/src/org/tzi/use/plugin/filmstrip/logic/FilmstripMVCompatibleExpressionVisitor.java
+++ FilmstripTransformation/trunk/src/org/tzi/use/plugin/filmstrip/logic/FilmstripMVCompatibleExpressionVisitor.java
@@ -7,9 +7,11 @@ import org.tzi.use.uml.mm.MAssociation;
 import org.tzi.use.uml.mm.MAssociationEnd;
 import org.tzi.use.uml.mm.MClass;
 import org.tzi.use.uml.mm.MModel;
+import org.tzi.use.uml.mm.MOperation;
 import org.tzi.use.uml.ocl.expr.ExpAllInstances;
 import org.tzi.use.uml.ocl.expr.ExpInvalidException;
 import org.tzi.use.uml.ocl.expr.ExpNavigation;
+import org.tzi.use.uml.ocl.expr.ExpObjOp;
 import org.tzi.use.uml.ocl.expr.ExpVariable;
 import org.tzi.use.uml.ocl.expr.Expression;
 import org.tzi.use.uml.ocl.expr.VarDecl;
@@ -31,46 +33,65 @@ public class FilmstripMVCompatibleExpressionVisitor extends
 		 * self.snapshot.<Class.asRolename()>
 		 */
 		MClass expType = (MClass) mc.mapType(exp.getSourceType());
+		
 		if(selfVariables.size() < 1){
 			throw new TransformationException("No self variable found for ExpAllInstances");
 		}
 		VarDecl selfDecl = selfVariables.varDecl(0);
 		Expression self = new ExpVariable(selfDecl.name(), selfDecl.type());
 		
+		MClass snapshotClass = model.getClass(FilmstripModelConstants.SNAPSHOT_CLASSNAME);
+		Expression snapshot;
+		
 		switch (type) {
 		case CLASSINVARIANT:
 		case OPERATION:
 		case SOIL:
-			// no changes required
+			// navigate from self to snapshot, self = random object in correct snapshot
+			MAssociation assoc = model.getAssociation(FilmstripModelConstants.makeSnapshotClsAssocName(((MClass) selfDecl.type()).name()));
+			
+			MAssociationEnd sourceEnd = assoc.associationEndsAt((MClass) selfDecl.type()).iterator().next();
+			MAssociationEnd destEnd = assoc.associationEndsAt(snapshotClass).iterator().next();
+			
+			try {
+				snapshot = new ExpNavigation(self, sourceEnd, destEnd, Collections.<Expression>emptyList());
+			} catch (ExpInvalidException e1) {
+				throw new TransformationException("ExpAllInstances: Unable to navigate to snapshot class", e1);
+			}
 			break;
 		case PRECONDITION:
 		case POSTCONDITION:
-			self = new ExpVariable(FilmstripModelConstants.OPC_SELF_VARNAME, expType);
-			if(type == ExpressionType.POSTCONDITION && !exp.isPre()){
-				self = FilmstripUtil.handlePredSucc(self, false, knownVariables);
+			// self is the operation call object
+			String opName = (type == ExpressionType.POSTCONDITION && !exp.isPre()) ? "succ" : "pred";
+			MOperation op = ((MClass) selfDecl.type()).operation(opName, true);
+			
+			try {
+				snapshot = new ExpObjOp(op, new Expression[]{ self });
+			} catch (ExpInvalidException e) {
+				throw new TransformationException("ExpAllInstances: Unable to call " + StringUtil.inQuotes(opName)
+						+ " on variable " + StringUtil.inQuotes(selfDecl.name())
+						+ " of type " + StringUtil.inQuotes(selfDecl.type()), e);
 			}
 			break;
 		default:
-			throw new TransformationException("Unkown expression type " + StringUtil.inQuotes(type));
+			throw new TransformationException("ExpAllInstances: Unkown expression type " + StringUtil.inQuotes(type));
 		}
 		
-		// navigate to snapshot and back to the class
-		MClass snapshot = model.getClass(FilmstripModelConstants.SNAPSHOT_CLASSNAME);
-		MAssociation assoc = model.getAssociation(FilmstripModelConstants
-				.makeSnapshotClsAssocName(expType.name()));
+		// navigate from snapshot to expType
+		MAssociation assoc2 = model.getAssociation(FilmstripModelConstants.makeSnapshotClsAssocName(expType.name()));
 		
-		MAssociationEnd sourceEnd = assoc.associationEndsAt(expType).iterator().next();
-		MAssociationEnd destEnd = assoc.associationEndsAt(snapshot).iterator().next();
-		Expression nav = self;
+		MAssociationEnd sourceEnd2 = assoc2.associationEndsAt(snapshotClass).iterator().next();
+		MAssociationEnd destEnd2 = assoc2.associationEndsAt(expType).iterator().next();
+		
+		Expression nav;
 		try {
-			if(type != ExpressionType.PRECONDITION && type != ExpressionType.POSTCONDITION){
-				nav = new ExpNavigation(nav, sourceEnd, destEnd,
-						Collections.<Expression>emptyList());
-			}
-			nav = new ExpNavigation(nav, destEnd,
-					sourceEnd, Collections.<Expression>emptyList());
-		} catch (ExpInvalidException ex) {
-			throw new TransformationException("ExpAllInstances", ex);
+			nav = new ExpNavigation(snapshot, sourceEnd2, destEnd2, Collections.<Expression>emptyList());
+		} catch (ExpInvalidException e) {
+			throw new TransformationException("ExpAllInstances: Unable to navigate from snapshot to objects", e);
+		}
+		
+		if(type == ExpressionType.POSTCONDITION && exp.isPre()){
+			nav = FilmstripUtil.handlePredSucc(nav, false, knownVariables);
 		}
 		
 		copyExpressionDetails(exp, nav);
