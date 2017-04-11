2157d5cacd81e7b07537f8074cf3c846a02efa78
fhilken <fhilken@e95982d2-0d12-e879-a455-8ce5a0885042>
2017-04-11 14:51:28 +0000

    - fixed a typo
    git-svn-id: https://db-svn.informatik.uni-bremen.de/repos/stuff/use_plugins@6210 e95982d2-0d12-e879-a455-8ce5a0885042

--- ModelValidator/branches/master-mariam/src/org/tzi/use/kodkod/plugin/ConfigurationComaprisonCmd.java
+++ ModelValidator/branches/master-mariam/src/org/tzi/use/kodkod/plugin/ConfigurationComaprisonCmd.java
@@ -4,6 +4,7 @@ import org.tzi.use.kodkod.compare.ConfigurationComparator;
 import org.tzi.use.main.shell.Shell;
 import org.tzi.use.main.shell.runtime.IPluginShellCmd;
 import org.tzi.use.runtime.shell.IPluginShellCmdDelegate;
+import org.tzi.use.uml.mm.MModel;
 
 public class ConfigurationComaprisonCmd implements IPluginShellCmdDelegate {
 
@@ -15,13 +16,15 @@ public class ConfigurationComaprisonCmd implements IPluginShellCmdDelegate {
 		
 		if(cmdArgumentList.length != 3){
 			shell.getErr().println("Wrong number of parameters.");
-			shell.getErr().println("Usage: " + pluginCommand.getCmd() + " <filname> <configname1> <configname1>");
+			shell.getErr().println("Usage: " + pluginCommand.getCmd() + " <filname> <configname1> <configname2>");
 			return;
 		}
 		
 		String filename = cmdArgumentList[0];
 		String config1 = cmdArgumentList[1];
 		String config2 = cmdArgumentList[2];
+		
+		MModel model = pluginCommand.getSession().system().model();
 		
 		System.out.println("filename: " + filename + "; c1: " + config1 + "; c2: " + config2);
 		// load file into HierarchicalINIConfiguration
